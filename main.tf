ad4145e6d9b92dba781d997a2aa2084765fdb28b
objectsforheads <objectsforheads@users.noreply.github.com>
2019-10-16 21:56:55 -0700

    add ssh_keys

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ resource "digitalocean_droplet" "www" {
   name = "www"
   region = "sfo2"
   size = "s-1vcpu-1gb"
+  ssh_keys = [
+    "66:0b:e5:21:cb:ce:c1:ae:f7:f0:16:d2:98:cc:bc:3d"
+  ]
   user_data = "${file("user_data.yaml")}"
 }
 
