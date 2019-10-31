0e52eb4b341d6e9e71b922191d37aa71b348c950
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2019-10-30 21:08:41 -0700

    List all assigned ip address + index of droplet

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "droplet_ip" {
-  value = "${digitalocean_droplet.droplet[0].ipv4_address}"
+  value = {
+    for index, droplet in digitalocean_droplet.droplets:
+    index => droplet.ipv4_address
+  }
+  # A list of ips can also be created using tf splat operator
+  # value = digitalocean_droplet.droplets.*.ipv4_address
 }
