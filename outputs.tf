77f7e9829a71aa7868f0734b06489f52e7e69180
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2019-11-01 07:00:19 -0700

    Added comment on for loop tf compatibility

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "droplet_ip" {
+  # NOTE: the for loop syntax is not backwards compatible with terraform <0.12.x.
   value = {
     for index, droplet in digitalocean_droplet.droplets:
     index => droplet.ipv4_address
