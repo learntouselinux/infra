64e874c38f568af5807507fde79731391b9981b1
Bri997 <brian.musial@gmail.com>
2019-11-14 20:48:26 -0500

    Have a static ip for the created droplet

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 provider "digitalocean" {}
 
 resource "digitalocean_droplet" "droplets" {
-  count = "${var.droplet_count}"
-  image = "${var.image}"
-  name = "${var.name}"
-  size = "${var.size}"
-  region = "${var.region}"
+  count     = "${var.droplet_count}"
+  image     = "${var.image}"
+  name      = "${var.name}"
+  size      = "${var.size}"
+  region    = "${var.region}"
   user_data = "${file("user-data.yaml")}"
-  ssh_keys = ["${var.ssh_fingerprint}"]
+  ssh_keys  = ["${var.ssh_fingerprint}"]
+}
+
+resource "digitalocean_floating_ip" "droplets" {
+  count      = "${var.droplet_count}"
+  droplet_id = "${element(digitalocean_droplet.droplets.*.id, count.index)}"
+  region     = "${var.region}"
 }
